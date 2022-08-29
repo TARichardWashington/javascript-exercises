c548816767fc11896e6b2b9c316ac5776cdfd088
Richard Washington <richard.washington@taxassist.co.uk>
2022-08-29 12:52:18 +0100

    :white_check_mark: pass 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeapYear = false;
 
+    if(year % 100 === 0 && year % 400 !== 0) {
+        return false;
+    }
+
+    isLeapYear = year % 4 === 0;
+    return isLeapYear;
 };
 
 // Do not edit below this line
