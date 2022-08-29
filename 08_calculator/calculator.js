c88fc32fa18b777db5331462e77e2db481adb5ae
Richard Washington <richard.washington@taxassist.co.uk>
2022-08-29 18:56:13 +0100

    :white_check_mark: pass 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  array.forEach(element => {
+    sum = sum + element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = array.pop();
+  array.forEach(element => {
+   total = total * element;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  //return num1 ** num2; // does not work with bigInts
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num -1);
+  }
+
 };
 
 // Do not edit below this line
