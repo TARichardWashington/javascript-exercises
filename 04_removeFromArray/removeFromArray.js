27e8283f5539a0284a11e51e4d9f819ceed6f4d1
Richard Washington <richard.washington@taxassist.co.uk>
2022-08-29 12:21:48 +0100

    :white_check_mark: pass 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
+    array = arguments[0];
+    let items = [];
 
+    for(let i = 1; i < arguments.length; i++) {
+        items.push(arguments[i]);
+    }
+
+    array = array.filter(arrayItem => !items.includes(arrayItem));
+    
+    return array;
 };
 
 // Do not edit below this line
