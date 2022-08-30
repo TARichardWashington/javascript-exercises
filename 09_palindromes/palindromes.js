d24a22f2e7233191f4481cb4dd7321c81da438cc
Richard Washington <richard.washington@taxassist.co.uk>
2022-08-30 21:21:22 +0100

    :white_check_mark: pass 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (forwardString) {
+    forwardString = forwardString.split('').
+                        filter(item => (item != '!' && item != ',' && item != ' ' && item != '.')).
+                        join('').toLowerCase();
+    
+    let backwardString = forwardString.split('').reverse().join('');
 
+    if(backwardString === forwardString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
